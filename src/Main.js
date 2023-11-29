261825ac705b0518750835e8f63e5546e5aef621
NIMIT <nimitj2003@gmail.com>
2023-11-29 14:04:26 +0530

    ....

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar/Navbar';
+import First from './components/First/First.js';
+import Second from './components/Second/Second.js';
+import Fifth from './components/Fifth/Fifth.js';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <First></First>
+    <Second></Second>
+    <Fifth></Fifth>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Second/Second.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Second.css';
+import AnimationPage2 from './AnimationPage2.jsx';
+
+function App() {
+
+  return (
+<div className="Secondm">
+<div className="Secondmbox">
+<AnimationPage2></AnimationPage2>
+<div className="Secondmboxtxt">
+
+</div>
+</div>
+</div>
+
+  );
+}
+
+export default App;
